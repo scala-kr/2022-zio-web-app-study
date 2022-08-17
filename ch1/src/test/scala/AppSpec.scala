94fbf82d75aec76513a80edecb77fd97ab53376b
som <solver.som@gmail.com>
2022-08-17 20:46:15 +0900

    테스트 ???로 변경

--- ch1/src/test/scala/AppSpec.scala
+++ ch1/src/test/scala/AppSpec.scala
@@ -1,20 +1,17 @@
-import sttp.client3._
-import sttp.client3.httpclient.zio.HttpClientZioBackend
 import zio.test._
 
 // 서버를 테스트해보세요
+// https://zio.dev/reference/test/
+// https://sttp.softwaremill.com/en/latest/quickstart.html
+// https://sttp.softwaremill.com/en/latest/backends/zio.html
 
 object AppSpec extends ZIOSpecDefault {
   override def spec = suite("App")(
     test("request test") {
-      val getGoogle = basicRequest.get(uri"https://google.com")
-      for {
-        backend <- HttpClientZioBackend()
-        resp <- getGoogle.send(backend)
-      } yield assertTrue(resp.code.code == 200)
+      assertTrue(true)
     },
     test("your test") {
-      assertTrue(true)
+      ???
     }
   )
 }
