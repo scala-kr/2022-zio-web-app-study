6ff4a847612b127767ed70ec9a1972aef54fc13b
Jisoo Park <xxxyel@gmail.com>
2022-09-28 20:59:11 +0900

    Hardcoded GET /todo/list

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,9 +5,19 @@ import zhttp.service.Server
 
 object Main extends ZIOAppDefault {
 
+  val todoList = List(
+    Todo(1, "scala study"),
+    Todo(2, "ZIO study")
+  )
+
   val httpApp = Http.collectZIO[Request] {
     case Method.GET -> !! / "hello" =>
       ZIO.succeed(Response.text("hello"))
+
+    case Method.GET -> !! / "todo" / "list" =>
+      ZIO.succeed(Response.json(
+        todoList.toJson
+      ))
   }
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
